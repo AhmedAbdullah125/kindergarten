51d11fe19a87e53d1177c82557d231bf561d0f62
AhmedAbdullah125 <ahmednabeeh98@gmail.com>
2024-04-21 00:00:51 +0200

    upload files

--- src/pages/Testimonials/Testimonials.jsx
+++ src/pages/Testimonials/Testimonials.jsx
@@ -17,15 +17,15 @@ import student10 from '../../assets/Testimonials/10.png'
 
 export default function Testimonials() {
     let testStudents = [
-        { id: 1, img: student1, name: "Hamza Ahmed",   openion: "Dieser Ort ist der Ort, den ich am meisten auf der Welt liebe. Ich liebe die Lehrer und ich liebe meine Kollegen" },
-        { id: 2, img: student2, name: "Hamza Ahmed",   openion: "Dieser Ort ist der Ort, den ich am meisten auf der Welt liebe. Ich liebe die Lehrer und ich liebe meine Kollegen" },
-        { id: 3, img: student3, name: "Hamza Ahmed",   openion: "Dieser Ort ist der Ort, den ich am meisten auf der Welt liebe. Ich liebe die Lehrer und ich liebe meine Kollegen" },
-        { id: 4, img: student4, name: "Hamza Ahmed",   openion: "Dieser Ort ist der Ort, den ich am meisten auf der Welt liebe. Ich liebe die Lehrer und ich liebe meine Kollegen" },
-        { id: 5, img: student5, name: "Hamza Ahmed",   openion: "Dieser Ort ist der Ort, den ich am meisten auf der Welt liebe. Ich liebe die Lehrer und ich liebe meine Kollegen" },
-        { id: 6, img: student6, name: "Hamza Ahmed",   openion: "Dieser Ort ist der Ort, den ich am meisten auf der Welt liebe. Ich liebe die Lehrer und ich liebe meine Kollegen" },
-        { id: 7, img: student7, name: "Hamza Ahmed",   openion: "Dieser Ort ist der Ort, den ich am meisten auf der Welt liebe. Ich liebe die Lehrer und ich liebe meine Kollegen" },
-        { id: 8, img: student8, name: "Hamza Ahmed",   openion: "Dieser Ort ist der Ort, den ich am meisten auf der Welt liebe. Ich liebe die Lehrer und ich liebe meine Kollegen" },
-        { id: 9, img: student9, name: "Hamza Ahmed",   openion: "Dieser Ort ist der Ort, den ich am meisten auf der Welt liebe. Ich liebe die Lehrer und ich liebe meine Kollegen" },
+        { id: 1, img: student1, name: "Hamza Ahmed", openion: "Dieser Ort ist der Ort, den ich am meisten auf der Welt liebe. Ich liebe die Lehrer und ich liebe meine Kollegen" },
+        { id: 2, img: student2, name: "Hamza Ahmed", openion: "Dieser Ort ist der Ort, den ich am meisten auf der Welt liebe. Ich liebe die Lehrer und ich liebe meine Kollegen" },
+        { id: 3, img: student3, name: "Hamza Ahmed", openion: "Dieser Ort ist der Ort, den ich am meisten auf der Welt liebe. Ich liebe die Lehrer und ich liebe meine Kollegen" },
+        { id: 4, img: student4, name: "Hamza Ahmed", openion: "Dieser Ort ist der Ort, den ich am meisten auf der Welt liebe. Ich liebe die Lehrer und ich liebe meine Kollegen" },
+        { id: 5, img: student5, name: "Hamza Ahmed", openion: "Dieser Ort ist der Ort, den ich am meisten auf der Welt liebe. Ich liebe die Lehrer und ich liebe meine Kollegen" },
+        { id: 6, img: student6, name: "Hamza Ahmed", openion: "Dieser Ort ist der Ort, den ich am meisten auf der Welt liebe. Ich liebe die Lehrer und ich liebe meine Kollegen" },
+        { id: 7, img: student7, name: "Hamza Ahmed", openion: "Dieser Ort ist der Ort, den ich am meisten auf der Welt liebe. Ich liebe die Lehrer und ich liebe meine Kollegen" },
+        { id: 8, img: student8, name: "Hamza Ahmed", openion: "Dieser Ort ist der Ort, den ich am meisten auf der Welt liebe. Ich liebe die Lehrer und ich liebe meine Kollegen" },
+        { id: 9, img: student9, name: "Hamza Ahmed", openion: "Dieser Ort ist der Ort, den ich am meisten auf der Welt liebe. Ich liebe die Lehrer und ich liebe meine Kollegen" },
         { id: 10, img: student10, name: "Hamza Ahmed", openion: "Dieser Ort ist der Ort, den ich am meisten auf der Welt liebe. Ich liebe die Lehrer und ich liebe meine Kollegen" },
     ]
     return (
@@ -34,7 +34,26 @@ export default function Testimonials() {
                 <h2>Unsere Erfahrungsberichte</h2>
             </div>
             <div className="slider1 slider">
-                <Swiper modules={[Autoplay]}
+                <Swiper 
+                breakpoints={{
+                    0: {
+                        // width: 0,
+                        slidesPerView: 1,
+                    },
+                    576: {
+                        // width: 576,
+                        slidesPerView: 1,
+                    },
+                    768: {
+                        // width: 768,
+                        slidesPerView: 1.3,
+                    },
+                    1200: {
+                        // width: 120,
+                        slidesPerView: 1.7,
+                    },
+                }}
+                modules={[Autoplay]}
                     autoplay={{ delay: 2500, pauseOnMouseEnter: true }}
                     spaceBetween={50}
                     slidesPerView={2.3}
@@ -44,7 +63,7 @@ export default function Testimonials() {
                     {testStudents.map((ele) => <>
                         {ele.id < 6 ?
                             <SwiperSlide>
-                                <div className="slide" key={ele}>
+                                <div className="slide slide1" key={ele}>
                                     <div className="text">
                                         <h3>{ele.name}</h3>
                                         <p>{ele.openion}</p>
@@ -61,24 +80,44 @@ export default function Testimonials() {
 
                 </Swiper>
             </div>
-            <div className=" slider">
-                <Swiper modules={[Autoplay]}
+            <div className="slider2 slider">
+                <Swiper
+                    breakpoints={{
+                        0: {
+                            // width: 0,
+                            slidesPerView: 1,
+                        },
+                        576: {
+                            // width: 576,
+                            slidesPerView: 1,
+                        },
+                        768: {
+                            // width: 768,
+                            slidesPerView: 1.3,
+                        },
+                        1200: {
+                            // width: 120,
+                            slidesPerView: 1.7,
+                        },
+                    }}
+                    modules={[Autoplay]}
                     autoplay={{ delay: 3500, pauseOnMouseEnter: true }}
+
                     spaceBetween={50}
                     slidesPerView={2.3}
                     onSlideChange={() => console.log('slide change')}
                     onSwiper={(swiper) => console.log(swiper)}
                 >
                     {testStudents.map((ele) => <>
-                        {ele.id >5 ?
+                        {ele.id > 5 ?
                             <SwiperSlide>
-                                <div className="slide" key={ele}>
+                                <div className="slide slide2"  key={ele}>
+                                <div className="img-cont">
+                                        <img src={ele.img} alt="student" />
+                                    </div>
                                     <div className="text">
                                         <h3>{ele.name}</h3>
                                         <p>{ele.openion}</p>
-                                    </div>
-                                    <div className="img-cont">
-                                        <img src={ele.img} alt="student" />
                                     </div>
                                 </div>
                             </SwiperSlide>
