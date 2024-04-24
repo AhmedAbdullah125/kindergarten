1d416246f58f62d602a2cfcdd2de737422343947
AhmedAbdullah125 <ahmednabeeh98@gmail.com>
2024-04-24 09:48:09 +0200

    upload files

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,16 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import icon from '../assets/ABC 16.png'
+import Favicon from 'react-favicon';
 
 
 
 export default function Layout() {
+    const [faviconUrl, setFaviconUrl] = useState({ icon });
+
     useEffect(() => {
         document.title = "A B C || أ ب ت"
     }, []);
     return (
         <Outlet>
-
+            <Favicon />
         </Outlet>
     )
 }
